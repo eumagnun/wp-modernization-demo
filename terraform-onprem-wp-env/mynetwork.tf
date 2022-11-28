d3a838bc221488a7bff2ddaf4a7026e2de37d6ce
Daniel da Silva Amaral <eumagnun@gmail.com>
2022-11-28 17:58:04 -0300

    Add files via upload

---
+++ terraform-onprem-wp-env/mynetwork.tf
@@ -0,0 +1,85 @@
+# Create the mynetwork network
+resource "google_compute_network" "mynetwork" {
+  name                    = "mynetwork"
+  routing_mode            = "GLOBAL"
+  auto_create_subnetworks = false
+}
+
+
+resource "google_compute_firewall" "mynetwork-allow-http" {
+  name    = "mynetwork-allow-http"
+  network = google_compute_network.mynetwork.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["web"]
+}
+
+resource "google_compute_firewall" "mynetwork-allow-database" {
+  name    = "mynetwork-allow-database"
+  network = google_compute_network.mynetwork.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["3306"]
+  }
+
+  target_tags = ["database"]
+  source_tags = ["web"]
+}
+
+resource "google_compute_firewall" "mynetwork-allow-ssh-from-cloudshell" {
+  name    = "mynetwork-allow-ssh-from-cloudshell"
+  network = google_compute_network.mynetwork.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["35.235.240.0/20"]
+}
+
+resource "google_compute_subnetwork" "subnet-southamerica-east1" {
+  name          = "subnet-southamerica-east1"
+  ip_cidr_range = "10.0.0.0/24"
+  region        = "southamerica-east1"
+  network       = google_compute_network.mynetwork.id
+}
+
+resource "google_compute_router_nat" "my-router-nat" {
+  name                               = "my-router-nat"
+  router                             = google_compute_router.my-router.name
+  region                             = google_compute_router.my-router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
+
+
+module "webapp-wp" {
+  source           = "./instance"
+  instance_name    = "backend-vm"
+  instance_zone    = "southamerica-east1-a"
+  instance_network = google_compute_network.mynetwork.self_link
+  instance_subnet  = google_compute_subnetwork.subnet-southamerica-east1.self_link
+  instance_network_tag = "web"
+  need_external_ip = true
+}
+
+module "database-wp" {
+  source           = "./instance"
+  instance_name    = "database-vm"
+  instance_zone    = "southamerica-east1-a"
+  instance_network = google_compute_network.mynetwork.self_link
+  instance_subnet  = google_compute_subnetwork.subnet-southamerica-east1.self_link
+  instance_network_tag = "database"
+}
+  
